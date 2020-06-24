6292c5fe5cffb62dd2755a2c2fe0a6e83961a3ee
mezahidali <m.zahiid@gmail.com>
2020-06-24 11:47:28 -0700

    Add files via upload

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+aws sts assume-role \
+    --role-arn arn:aws:iam::711451427668:role/AWSGlueCICD-CodePipelineServiceRole-REX9XUCQD8RZ \
+    --role-session-name deploy \
+    --query 'Credentials.{AWS_ACCESS_KEY_ID:AccessKeyId, AWS_SECRET_ACCESS_KEY:SecretAccessKey, AWS_SESSION_TOKEN:SessionToken}'  | grep AWS | tr -d ' ",' | tr : = | sed -e 's/^/export /' > /tmp/creds
+
+echo -n creds are; cat /tmp/creds
