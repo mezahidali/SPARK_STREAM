bf1f72fb3125cd358df2147476ec32a2b04bbab1
mezahidali <m.zahiid@gmail.com>
2020-06-25 09:42:33 -0700

    Add files via upload

--- deploy_glue.sh
+++ deploy_glue.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 DIRNAME=$(dirname "$0")
 mkdir $DIRNAME/output
-$S3_BUCKET="aws-zhidli"
+S3_BUCKET="aws-zhidli"
 aws cloudformation package --template-file $DIRNAME/template-glue.yaml --s3-bucket $S3_BUCKET --s3-prefix CFT-Folder/ --output-template-file $DIRNAME/output/packaged-template.yaml
